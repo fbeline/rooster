d89c91d2e2d9b86199cd8b4ab9ac37e7c47ce98a
Felipe Baravieira <fe.belineb@gmail.com>
2017-02-13 22:23:42 -0200

    test fixes

--- test/dispatcher_test.erl
+++ test/dispatcher_test.erl
@@ -2,7 +2,7 @@
 -include_lib("../src/rooster.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--export([route_function/1, route_function_pparam/1]).
+-export([route_function/2, route_function_pparam/2]).
 
 compare_route_tokens_simple_test() ->
 	RequestedRoute = ["products", "save"],
@@ -42,17 +42,17 @@ parse_route_path_params_test() ->
 call_route_function_body_test() ->
     Body = "{\n\t\"user\": \"felipe 1234\"\n}",
     Req = #request{body=Body},
-    Resp = rooster_dispatcher:call_route_function(Req, {?MODULE, route_function}, {}),
+    Resp = rooster_dispatcher:call_route_function(Req, {?MODULE, route_function}, {}, []),
     ?assertEqual({[{<<"user">>, <<"felipe 1234">>}]}, Resp).
 
 call_route_function_pparam_test() ->
     Params = [{param1, 10}, {param2, 20}],
     Req = #request{},
-    Resp = rooster_dispatcher:call_route_function(Req, {?MODULE, route_function_pparam}, Params),
+    Resp = rooster_dispatcher:call_route_function(Req, {?MODULE, route_function_pparam}, Params, []),
     ?assertEqual(Params, Resp).
 
-route_function(Req) ->
+route_function(Req, _Resp) ->
     Req#request.body.
 
-route_function_pparam(Req) ->
+route_function_pparam(Req, _Resp) ->
     Req#request.pathParams.
