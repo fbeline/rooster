e7c9b84bd59c3d5a9c484d7ba23d730b54019e21
felipebb <fe.belineb@gmail.com>
2017-10-01 13:21:48 -0300

    adapter unit test

---
+++ test/adapter_test.erl
@@ -0,0 +1,22 @@
+-module(adapter_test).
+-author("felipe").
+
+-include_lib("eunit/include/eunit.hrl").
+
+simple_test() ->
+  ?assert(true).
+
+config_adapter_test() ->
+  Expected = #{ip          => {0, 0, 0, 0},
+               port        => 8080,
+               static_path => ["priv", "www"],
+               ssl         => {ssl, false},
+               ssl_opts    => {ssl_opts, []}},
+  ?assertEqual(Expected, rooster_adapter:config(#{})).
+
+state_adapter_test() ->
+  Expected = #{routes       => [],
+               middleware   => [],
+               resp_headers => [],
+               version      => "0.0.0"},
+  ?assertEqual(Expected, rooster_adapter:state(#{})).
