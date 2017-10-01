0f42ca1b4eb735b620707e4f738072663732c8ad
felipebb <fe.belineb@gmail.com>
2017-10-01 13:10:45 -0300

    code indentation

--- include/rooster.hrl
+++ include/rooster.hrl
@@ -1,22 +1,22 @@
--type state() :: #{routes => list(atom()),
-                    middleware => list(atom()),
-                    resp_headers => list({string(), string()}),
-                    version => string()}.
+-type state() :: #{routes       => list(atom()),
+                   middleware   => list(atom()),
+                   resp_headers => list({string(), string()}),
+                   version      => string()}.
 
--type request() :: #{path => string(),
-                    method => atom(),
-                    headers => any(),
-                    body => any(),
-                    qs => any(),
-                    cookies => any(),
-                    authorization => string(),
-                    pathParams => list()}.
+-type request() :: #{path          => string(),
+                     method        => atom(),
+                     headers       => any(),
+                     body          => any(),
+                     qs            => any(),
+                     cookies       => any(),
+                     authorization => string(),
+                     pathParams    => list()}.
 
--type config() :: #{ip => {integer(), integer(), integer(), integer()},
-                   port => integer(),
-                   static_path => list(string()),
-                   ssl => any(),
-                   ssl_opts => any()}.
+-type config() :: #{ip          => {integer(), integer(), integer(), integer()},
+                    port        => integer(),
+                    static_path => list(string()),
+                    ssl         => any(),
+                    ssl_opts    => any()}.
 
 -type route() :: {module(), atom(), string(), any()}.
 -type middleware() :: {module(), atom(), string(), any()}.
