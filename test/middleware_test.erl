5e5de9199c8773bc70fd14f98a025bf0ef481676
felipebb <fe.belineb@gmail.com>
2017-10-28 23:22:47 -0200

    middleware
    Middleware can break the chain of executions at any time and return a server response.

--- test/middleware_test.erl
+++ test/middleware_test.erl
@@ -5,7 +5,8 @@
 middleware() ->
   [#{name => foo, leave => fun(Resp) -> Resp * 2 end},
    #{name => bar, enter => fun(Req) -> Req * 3 end},
-   #{name => baz, enter => fun(Req) -> Req * 2 end, leave => fun(Resp) -> Resp * 2 end}].
+   #{name => baz, enter => fun(Req) -> Req * 2 end, leave => fun(Resp) -> Resp * 2 end},
+   #{name => breaker, enter => fun(_Req) -> {break, {404, #{}}} end}].
 
 server_start_test() ->
   Middleware = middleware(),
@@ -17,19 +18,24 @@ server_start_test() ->
 leave_test() ->
   rooster_middleware:start_link(middleware()),
   Resp = rooster_middleware:leave(1, [foo]),
-  ?assertEqual(2, Resp).
+  ?assertEqual({ok, 2}, Resp).
 
 enter_test() ->
   rooster_middleware:start_link(middleware()),
   Req = rooster_middleware:enter(1, [bar]),
-  ?assertEqual(3, Req).
+  ?assertEqual({ok, 3}, Req).
 
 multiple_middleware_enter_test() ->
   rooster_middleware:start_link(middleware()),
   Req = rooster_middleware:enter(1, [baz, bar]),
-  ?assertEqual(6, Req).
+  ?assertEqual({ok, 6}, Req).
 
 multiple_middleware_leave_test() ->
   rooster_middleware:start_link(middleware()),
   Resp = rooster_middleware:leave(1, [baz, foo]),
-  ?assertEqual(4, Resp).
+  ?assertEqual({ok, 4}, Resp).
+
+break_middleware_test() ->
+  Resp = rooster_middleware:enter(1, [breaker, foo]),
+  ?assertEqual({break, {404, #{}}}, Resp).
+
