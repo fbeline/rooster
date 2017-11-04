53f7d53e5e27283ba870182f6288f3857caedd55
felipebb <fe.belineb@gmail.com>
2017-11-04 15:43:04 -0200

    refactor refact compare_route_tokens fn

--- src/rooster_dispatcher.erl
+++ src/rooster_dispatcher.erl
@@ -1,21 +1,21 @@
 -module(rooster_dispatcher).
 
--export([match_route/2, compare_route_tokens/3, parse_route/1, handle_request/3]).
+-export([match_route/2]).
+
+-ifdef(TEST).
+-compile(export_all).
+-endif.
 
 match_route(#{path := Path, method := Method} = Req, Routes) ->
   match_route(Path, Method, Req, Routes).
 
-match_route(_, _, _Req, []) -> {404, #{message => <<"Not found">>}};
+match_route(_, _, _, []) -> {404, #{message => <<"Not found">>}};
 match_route(RequestedRoute, Method, Req, [{Method, Route, Fn, Middleware} | T]) ->
-  RouteTokens = parse_route(Route),
-  RequestedRouteTokens = parse_route(RequestedRoute),
-  {IsValid, Params} = compare_route_tokens(RouteTokens, RequestedRouteTokens, #{}),
-  if IsValid =:= true ->
-    handle_request(Req#{params := Params}, Fn, Middleware);
-    true ->
-      match_route(RequestedRoute, Method, Req, T)
-  end;
-match_route(Route, M1, Req, [_ | T]) -> match_route(Route, M1, Req, T).
+  {ContextTokens, RequestTokens} = {parse_route(Route), parse_route(RequestedRoute)},
+  case path_params(ContextTokens, RequestTokens) of
+    {ok, Params} ->  handle_request(Req#{params := Params}, Fn, Middleware);
+    {not_match, _} -> match_route(RequestedRoute, Method, Req, T)
+  end.
 
 handle_request(Request, Fn, Middleware) ->
   Res = rooster_middleware:enter(Request, Middleware),
@@ -32,18 +32,17 @@ parse_route(Route) ->
   RouteTokens = string:tokens(RouteWithoutQueryParams, "/"),
   RouteTokens.
 
-compare_route_tokens([], [], Acc) -> {true, Acc};
-compare_route_tokens([], [_H | _T], _) -> {false, {}};
-compare_route_tokens([_H | _T], [], _) -> {false, {}};
-compare_route_tokens([H1 | T1], [H2 | T2], Acc) ->
-  IsPathParam = string:str(H1, ":") =:= 1,
-  SameToken = H1 =:= H2,
-  if IsPathParam ->
-    compare_route_tokens(T1, T2, new_param(Acc, {H1, H2}));
-    SameToken ->
-      compare_route_tokens(T1, T2, Acc);
-    true ->
-      {false, {}}
+path_params(ContextTokens, RequestedTokens) ->
+  if erlang:length(ContextTokens) =:= erlang:length(RequestedTokens) -> path_params(ContextTokens, RequestedTokens, #{});
+    true -> {not_match, #{}}
+  end.
+
+path_params([], [], Acc) -> {ok, Acc};
+path_params([Token|T1], [Token|T2], Acc) -> path_params(T1, T2, Acc);
+path_params([Context|T1], [Requested|T2], Acc) ->
+  IsPathParam = string:str(Context, ":") =:= 1,
+  if IsPathParam -> path_params(T1, T2, new_param(Acc, {Context, Requested}));
+     true -> {not_match, #{}}
   end.
 
 new_param(Params, {Key, Value}) ->
